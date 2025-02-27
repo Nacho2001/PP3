4ef0e4e3e239d41fed89ea83fe1f379a27e77e52
Nacho2001 <nachomigoni@gmail.com>
2025-02-27 20:22:41 -0300

    limpeza previa

--- api/userLogin/routes/registerRoute.js
+++
@@ -1,9 +0,0 @@
-// Dependencias: express, enrutador y controller de usuario
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/userController');
-
-// Crear usuario nuevo
-router.post('/', controller.createUser);
-
-module.exports = router;
